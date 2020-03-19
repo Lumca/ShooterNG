966def6cdecf537741f66458c90bd5a081c25317
Lumír <lumcak@gmail.com>
2020-03-20 00:57:03 +0100

    Added alot

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+    void Awake()
+    {
+        GameObject[] objects = GameObject.FindGameObjectsWithTag("music");
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    void Start()
+    {
+        Invoke("LoadFirstScene", 2f);
+    }
+
+    void LoadFirstScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    [Tooltip(("in ms^-1"))] [SerializeField] float xSpeed = 45f;
+    [Tooltip(("in ms^-1"))] [SerializeField] float ySpeed = 45f;
+    [SerializeField] float xEdge = 15f;
+    [SerializeField] float yEdge = 7f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movementHorizontal();
+        movementVertical();
+        void movementHorizontal()
+        {
+            float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+            float xOffset = xThrow * xSpeed * Time.deltaTime;
+            float rawNewXPos = transform.localPosition.x + xOffset;
+            float clampXPos = Mathf.Clamp(rawNewXPos, -xEdge, xEdge);
+            transform.localPosition = new Vector3(clampXPos, transform.localPosition.y, transform.localPosition.z);
+        }
+        
+        void movementVertical()
+        {
+            float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+            float yOffset = yThrow * ySpeed * Time.deltaTime;
+            float rawNewYPos = transform.localPosition.y + yOffset;
+            float clampYPos = Mathf.Clamp(rawNewYPos, -yEdge, yEdge);
+            transform.localPosition = new Vector3(transform.localPosition.x, clampYPos, transform.localPosition.z);
+            
+            transform.localRotation = Quaternion.Euler(0,0,0);
+        }
+        
+    }
+}
